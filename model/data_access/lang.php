a85133dad3eb8977d526093422e37b724ff62722
amirpashaie <mr.amirpashaie@gmail.com>
2017-06-25 11:41:54 +0430

    language class for message

---
+++ model/data_access/lang.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amir's dell
+ * Date: 6/21/2017
+ * Time: 7:14 PM
+ */
+namespace model\data_access;
+
+class lang{
+
+    public static $success_login = "ورود با موفقیت انجام شد.";
+    public static $wrong_login = "رمز عبور و یا نام کاربری اشتباه وارد شده است.";
+    public static $not_exist_login = "نام کاربری مورد نظر وجود ندارد.";
+    public static $success = "عملیات با موفقیت انجام شد.";
+    public static $failed = "عملیات نا موفق بود.";
+    public static $error_change_pass = "رمز عبور قبلی صحیح نمی باشد.";
+    public static $error_file = "خطایی در آپلود رخ داده است.";
+    public static $file_not_set = "تصویر ارسال شده وجود ندارد.";
+    public static $not_data_here = "اطلاعاتی وجود ندارد.";
+    public static $invalid_data  = 'اطلاعات معتبر نمی باشد';
+    public static $invalid_command  = 'دستور مورد نظر اشتباه می باشد.';
+
+    public static $message = "پیغام!";
+    public static $error   = "خطا!";
+    public static $success_type = "success";
+    public static $error_type   = "error";
+    public static $info_type    = "info";
+
+}
